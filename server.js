7abf91209d359278504e2df881cdf3066c8da451
Harinim-h <harinim378@gmail.com>
2024-12-06 18:36:40 +0530

    poll

--- server.js
+++ server.js
@@ -106,3 +106,6 @@ app.delete('/api/polls/:id', (req, res) => {
 
 // Server setup
 const port = process.env.PORT || 5000;
+app.listen(port, () => {
+    console.log(Server running on port ${port});
+  });
